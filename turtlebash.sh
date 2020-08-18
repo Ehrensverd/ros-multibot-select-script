ab4697393adf206347e4e963f7ed388b97df08ca
Ehrensverd <mail.eskil@gmail.com>
2020-08-19 00:54:32 +0200

    Add shell info msg

--- turtlebash.sh
+++ turtlebash.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+# TODO add more sys args for changing multiple robots
 python3 /home/eskil/ros-multibot-script/turtler.py $1;
 export TURTLEBOT3_MODEL=$(cat .current_turtle);
+# Echo current serrsion var
+changed_variable="TURTLEBOT3_MODEL variable now set to:";
+echo "${changed_variable} $(cat .current_turtle)"
